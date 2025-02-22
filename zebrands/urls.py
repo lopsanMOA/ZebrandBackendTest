575e0e09efd98ce7f9565ca846375a8e4ab997d8
LopsanAMO <lopsan.molina@gmail.com>
2025-02-21 20:50:57 -0600

    update urls

--- zebrands/urls.py
+++ zebrands/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('', include('products.api.v1.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/doc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
